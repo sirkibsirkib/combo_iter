ffa115d1bfb1277005adc56bbc04f74f24c785b8
Christopher Esterhuyse <christopher.esterhuyse@gmail.com>
2023-07-18 23:36:09 +0200

    main

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,14 @@
 mod iter;
-mod test;
 
-pub trait ComboSliceLike<'a, T: 'a>: AsRef<[&'a T]> + AsMut<[&'a T]> {}
-impl<'a, T: 'a> ComboSliceLike<'a, T> for Box<[&'a T]> {}
-impl<'a, T: 'a, const N: usize> ComboSliceLike<'a, T> for [&'a T; N] {}
+#[cfg(test)]
+mod test;
 
 pub struct ComboIter<'a, T, S: AsRef<[*const T]> + AsMut<[*const T]>> {
     pub items: &'a [T],
-    // INVARIANT: if let State::Next(ptrs) = self.state,
-    // then each in ptrs is aligned to an element in self.items.
+    // INVARIANT: pointers in self.next.as_ref() are aligned to items
     pub(crate) next: Option<S>,
     pub ever_advanced: bool,
 }
 
 pub type BoxComboIter<'a, T> = ComboIter<'a, T, Box<[*const T]>>;
 pub type ArrayComboIter<'a, T, const C: usize> = ComboIter<'a, T, [*const T; C]>;
-
-fn main() {
-    let mut x = ArrayComboIter::<_, 2>::new(&[0, 1, 2]);
-    while let Some(combo) = x.next() {
-        println!("{:?}", combo);
-    }
-}
--- src/test.rs
+++ src/test.rs
@@ -1,14 +1,16 @@
-use crate::BoxComboIter;
+use crate::ComboIter;
 
-fn check_boxed<const C: usize>(num_items: u16, expect_combos: &[[u16; C]]) {
-    fn check_combo(a: &[&u16], b: &[u16]) {
-        assert!(a.len() == b.len());
-        for (a, b) in a.iter().zip(b.iter()) {
-            assert_eq!(*a, b)
-        }
+fn check_combo(a: &[&u16], b: &[u16]) {
+    assert!(a.len() == b.len());
+    for (a, b) in a.iter().zip(b.iter()) {
+        assert_eq!(*a, b)
     }
-    let items: Vec<_> = (0u16..num_items).collect();
-    let mut combo_iter = BoxComboIter::new(&items, C);
+}
+
+fn check<const C: usize, S: AsRef<[*const u16]> + AsMut<[*const u16]>>(
+    mut combo_iter: ComboIter<u16, S>,
+    expect_combos: &[[u16; C]],
+) {
     let mut expect_combo_iter = expect_combos.iter();
     while let Some(a) = combo_iter.next() {
         let b = expect_combo_iter.next().unwrap();
@@ -17,42 +19,98 @@ fn check_boxed<const C: usize>(num_items: u16, expect_combos: &[[u16; C]]) {
     assert!(expect_combo_iter.next().is_none());
 }
 
-#[test]
-fn a() {
-    check_boxed(1, &[[0]])
+mod boxed {
+    fn check<const C: usize>(num_items: u16, expect_combos: &[[u16; C]]) {
+        let items: Vec<_> = (0u16..num_items).collect();
+        let combo_iter = crate::BoxComboIter::new(&items, C);
+        super::check(combo_iter, expect_combos)
+    }
+
+    #[test]
+    fn a() {
+        check(1, &[[0]])
+    }
+
+    #[test]
+    fn b() {
+        check(1, &[[]])
+    }
+
+    #[test]
+    fn c() {
+        check(0, &[[]])
+    }
+
+    #[test]
+    fn d() {
+        check(2, &[[0], [1]])
+    }
+
+    #[test]
+    fn e() {
+        check(2, &[[0, 0], [0, 1], [1, 0], [1, 1]])
+    }
+
+    #[test]
+    fn f() {
+        check(3, &[[]])
+    }
+
+    #[test]
+    fn g() {
+        check(3, &[[0], [1], [2]])
+    }
+
+    #[test]
+    fn h() {
+        check(3, &[[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]])
+    }
 }
 
-#[test]
-fn b() {
-    check_boxed(1, &[[]])
-}
+mod arrayed {
+    fn check<const C: usize>(num_items: u16, expect_combos: &[[u16; C]]) {
+        let items: Vec<_> = (0u16..num_items).collect();
+        let combo_iter = crate::ArrayComboIter::<_, C>::new(&items);
+        super::check(combo_iter, expect_combos)
+    }
 
-#[test]
-fn c() {
-    check_boxed(0, &[[]])
-}
+    #[test]
+    fn a() {
+        check(1, &[[0]])
+    }
 
-#[test]
-fn d() {
-    check_boxed(2, &[[0], [1]])
-}
+    #[test]
+    fn b() {
+        check(1, &[[]])
+    }
 
-#[test]
-fn e() {
-    check_boxed(2, &[[0, 0], [0, 1], [1, 0], [1, 1]])
-}
+    #[test]
+    fn c() {
+        check(0, &[[]])
+    }
 
-#[test]
-fn f() {
-    check_boxed(3, &[[]])
-}
+    #[test]
+    fn d() {
+        check(2, &[[0], [1]])
+    }
 
-#[test]
-fn g() {
-    check_boxed(3, &[[0], [1], [2]])
-}
+    #[test]
+    fn e() {
+        check(2, &[[0, 0], [0, 1], [1, 0], [1, 1]])
+    }
 
-#[test]
-fn h() {
-    check_boxed(3, &[[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]])
+    #[test]
+    fn f() {
+        check(3, &[[]])
+    }
+
+    #[test]
+    fn g() {
+        check(3, &[[0], [1], [2]])
+    }
+
+    #[test]
+    fn h() {
+        check(3, &[[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]])
+    }
 }
